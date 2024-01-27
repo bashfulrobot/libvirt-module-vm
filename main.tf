8a4ddd1cf899b6a1a1c151237fce2d6bfb12f072
bashfulrobot <dustin@bashfulrobot.com>
2024-01-26 23:50:37 -0800

    chore: regular vm. strip the old runcmd

--- main.tf
+++ main.tf
@@ -71,11 +71,6 @@ users:
 ssh_pwauth: True
 runcmd:
   - [ bash, -c, 'echo "Cloud-init start: $(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z")" >> /home/${var.admin_name}/cloud-init-run.log' ]
-  - [ bash, -c, 'start_time=$(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z"); echo "Running Wget - install-controller.sh: $start_time" >> /home/${var.admin_name}/cloud-init-run.log' ]
-  - [ bash, -c, 'wget -P /tmp https://raw.githubusercontent.com/bashfulrobot/libvirt-module-helpers/main/install-controller.sh >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
-  - [ bash, -c, 'start_time=$(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z"); echo "Running Wget - miniserve: $start_time" >> /home/${var.admin_name}/cloud-init-run.log' ]
-  - [ bash, -c, 'wget -P /tmp https://raw.githubusercontent.com/bashfulrobot/libvirt-module-helpers/main/miniserve >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
-  - [ bash, -c, 'chmod +x /tmp/miniserve /tmp/install-controller.sh >> /home/${var.admin_name}/cloud-init-run.log 2>&1' ]
   - [ bash, -c, 'echo "Cloud-init end: $(TZ=":America/Vancouver" date "+%Y-%m-%d %H:%M:%S.%N %Z")" >> /home/${var.admin_name}/cloud-init-run.log' ]
 EOF
 }
